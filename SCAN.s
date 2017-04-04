f511ea151b55fd757bee4f5d6cfd9a49c1be6686
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2017-04-04 14:35:25 -0700

    12 ARM Assembly Programs

---
+++ SCAN.s
@@ -0,0 +1,12 @@
+		;		SCAN
+		;		Kevin Carlos
+		;		4 April 2017
+		;		v1.00
+		;
+		;		Length stored in 0x0158
+		;		List begins at 0x0160
+		Fill		4
+		Fill		4 ; to get memory at
+		;		data
+LLST		dcd		4781
+		END
