37a62bdfe9ab832fa254b071f97fcb27e127cade
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2017-04-04 17:00:41 -0700

    updated scan

--- COMBINE.s
+++ COMBINE.s
@@ -3,13 +3,14 @@
 		;		4 April 2017
 		;		v1.00
 		;
-		
 		ldr		r0, =value1
 		ldr		r1, [r0]
 		
+		
+		
 		;		data
-value1	dcb		5, 6
-value2	dcb		10, 11
-value3	dcb		15, 16
-value4	dcb		20, 21
+value1	dcd		500
+value2	dcd		600
+value3	dcd		700
+value4	dcd		800
 		END
--- SCAN.s
+++ SCAN.s
@@ -5,8 +5,12 @@
 		;
 		;		Length stored in 0x0158
 		;		List begins at 0x0160
-		Fill		4
-		Fill		4 ; to get memory at
+		ldr		r0, =LLST ;point to list length
+		ldr		r1, [r0]	;
 		;		data
-LLST		dcd		4781
+SMALLV	dcb		0 		;at 0x0150
+MAXV		dcb		0		;at 0x0154
+LLST		dcb		10		;at 0x0158
+buffer	dcb		0		;at 0x015C
+LIST		dcb		45, 60, 11, 7, 8, 6, 4, 7, 6, 13, 0	;0x0160
 		END
